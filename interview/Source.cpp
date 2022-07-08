cd83f62c58447bd4f5067523ea6e582e030b633e
ChihhaoFeng <79551034+ChihhaoFeng@users.noreply.github.com>
2022-07-08 14:11:53 +0800

    Function:
    StringLength(input);
    
    SameCharNum(input, 'p');
    
    RandomSingleSort(10);
    
    TwoSum(nums, 10);
    
    Reverse(nums);
    
    PrimeFactor();
    
    ReverseSentence(sen);
    
    Password();

--- interview/Source.cpp
+++ interview/Source.cpp
@@ -11,7 +11,8 @@ class Solution {
 public: 
     static int StringLength( string args ){
         /// <summary>
-        /// 最後一個單詞的長度
+        /// 计算字符串最后一个单词的长度，单词以空格隔开。  输入描述：一行字符串，非空，长度小于5000。
+        /// 输出描述: 整数N，最后一个单词的长度。
         /// </summary>
 
         int max = 0;
@@ -42,7 +43,7 @@ public:
     static int SameCharNum(string args, char s) 
     {
         /// <summary>
-        /// string中,指定字母的數量
+        /// 接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
         /// </summary>
 
         int num = 0;
@@ -88,7 +89,7 @@ public:
             cout << n << " ";
         }
 
-        removeDuplicates(list);
+        removeSortedDuplicates(list);
 
         cout << endl;
         for (int n : list)
@@ -168,6 +169,163 @@ public:
         return result;
     }
 
+    static vector<int> Reverse(vector<int>& input)
+    {
+        ///<summary>
+        ///输入一个int型整数，按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
+        ///<summary>
+
+        unordered_map<int, int> hash;
+        vector<int> output;
+
+        for (int i = input.size()-1 ; i >= 0; i--)
+        {
+            if (hash.find(input[i]) != hash.end())
+            {
+                continue;
+            }
+
+            hash[input[i]] = i;
+            output.push_back(input[i]);
+            cout << input[i] << " ";
+        }
+
+        return output;
+
+    }
+
+    static vector<char> ReverseSentence(string sen) 
+    {
+        ///<summary>
+        /// 将一个英文语句以单词为单位逆序排放。例如“I am a boy”，逆序排放后为“boy a am I”
+        /// 所有单词之间用一个空格隔开，语句中除了英文字母外，不再包含其他字符
+        ///<summary>
+        
+        vector<char> output;
+
+        bool blank = false;
+        int count = 0;
+        int pointer = sen.size() - 1;
+        int tmp;
+
+
+        while (pointer >= 0) 
+        {
+            if (sen[pointer] == ' ' && blank == false)
+            {
+                pointer--;
+            }
+            else if (sen[pointer] == ' ' && blank == true) 
+            {
+                output.push_back(' ');
+                cout << ' ';
+                blank = false;
+            }
+            else if (sen[pointer] != ' ')
+            {   
+                count = 0;
+                blank = true;
+                while (sen[pointer] != ' ')   // 抓到單字,回傳單字長度
+                {
+                    count++;
+                    pointer--;
+                }
+                tmp = pointer;
+
+                for (int i = 0; i < count+1; i++) 
+                {
+                    output.push_back(sen[tmp+i]);
+                    cout << sen[tmp + i];
+                }
+            }
+            
+        }
+
+
+
+        return output;
+
+    }
+
+    static int majorityElement(vector<int>& nums) {
+
+
+        /// <summary>
+        /// The majority element is the element that appears more than ⌊n / 2⌋ times. 
+        /// You may assume that the majority element always exists in the array.
+        /// ex:
+        /// Input: nums = [2,2,1,1,1,2,2]
+        /// Output: 2
+        /// </summary>
+
+        int major = nums[0];
+
+        int count = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (major == nums[i])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+
+            if (count < 0)
+            {
+                major = nums[i];
+                count = 0;
+            }
+
+        }
+
+        return major;
+
+    }
+    
+    static void Password()
+    {
+        /// <summary>
+        /// 密码验证合格程序
+        /// </summary>    
+        
+        
+        string password = "Danielfeng";
+
+        string input;
+        bool pass = false;
+
+        while (!pass) 
+        {
+            pass = true;
+
+            cout << "Please input your password: ";
+            cin >> input;
+
+            for (int i = 0; i < password.size(); i++)
+            {
+                if (password[i] != input[i])
+                {
+                    pass = false;
+                    break;
+                }
+            }
+
+            if (pass)
+            {
+                cout << "pass" <<endl;
+            }
+            else
+            {
+                cout << "error"<< endl;
+            }
+        }
+        
+
+
+    }
 
 
 
@@ -181,7 +339,7 @@ private:
                     swap(input[j], input[j + 1]);
     }
 
-    static void removeDuplicates(vector<int>& nums) 
+    static void removeSortedDuplicates(vector<int>& nums) 
     {
         int j = 1;
         if (nums.size() == 0 || nums.size() == 1)
@@ -202,18 +360,54 @@ private:
         return;
     }
 
+    static vector<string> split(const string& s, const string& seperator) {
+        
+        vector<string> result;
+        typedef string::size_type string_size;
+        string_size i = 0;
 
+        while (i != s.size()) {
+            //找到字符串中首个不等于分隔符的字母；
+            int flag = 0;
+            while (i != s.size() && flag == 0) {
+                flag = 1;
+                for (string_size x = 0; x < seperator.size(); ++x)
+                    if (s[i] == seperator[x]) {
+                        ++i;
+                        flag = 0;
+                        break;
+                    }
+            }
 
-
-
-
+            //找到又一个分隔符，将两个分隔符之间的字符串取出；
+            flag = 0;
+            string_size j = i;
+            while (j != s.size() && flag == 0) {
+                for (string_size x = 0; x < seperator.size(); ++x)
+                    if (s[j] == seperator[x]) {
+                        flag = 1;
+                        break;
+                    }
+                if (flag == 0)
+                    ++j;
+            }
+            if (i != j) {
+                result.push_back(s.substr(i, j - i));
+                i = j;
+            }
+        }
+        return result;
+    }
 };
 
 
 int main()
 {
     string input = "people4ar6es5go4ophpfp4pdpph4fdd";
+    string sen = "   observe dont you     but see You     ";
 
+   
+    vector<char> output;
     srand(time(NULL));
 
     //Solution::StringLength(input);
@@ -222,16 +416,17 @@ int main()
 
     //Solution::RandomSingleSort(10);
 
-    //vector<int> twosum = { 2,7,4,5,5,7,8 };
-    //Solution::TwoSum(twosum, 10);
+    vector<int> nums = { 2,7,4,5,5,7,8 };
+    //Solution::TwoSum(nums, 10);
+
+
+    //Solution::Reverse(nums);
     
     //Solution::PrimeFactor();
 
+    //Solution::ReverseSentence(sen);
 
-
-
-
-
+    //Solution::Password();
 
     return 0;
 }
